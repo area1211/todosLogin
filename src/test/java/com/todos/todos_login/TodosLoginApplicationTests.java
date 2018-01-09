5d33183b56599363abd07299aeaa5fc3a0b225fa
kevin@todos.co.kr <player16@!>
2018-01-09 12:09:16 +0900

    first commit

---
+++ src/test/java/com/todos/todos_login/TodosLoginApplicationTests.java
@@ -0,0 +1,16 @@
+package com.todos.todos_login;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TodosLoginApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
